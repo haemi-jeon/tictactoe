d7ffa801969b8ec2b58626349592bdb4592aeaa4
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2023-06-29 17:26:59 +0900

    리액트 익히기

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,15 +3,28 @@ import Square from './Square';
 import './Board.css';
 
 export default class Board extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
   render() {
-    const status = 'Next Player: X, O';
     return (
       <div>
-        <div className="status">{status}</div>
+        <div className="status">Next Player: X, O</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
