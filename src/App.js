a0feff9a44490b99325da441ce9182e8a27f9f71
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2023-06-30 16:36:38 +0900

    중간

--- src/App.js
+++ src/App.js
@@ -2,12 +2,57 @@ import './App.css';
 import Board from './components/Board';
 
 function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = squares => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  const handleClick = i => {
+    const newSquares = current.squares.slice();
+    if (calculateWinner(newSquares) || squares[i]) {
+      return;
+    }
+  };
+
   return (
     <div className="game">
       <div className="game-board">
         <Board />
       </div>
-      <div className="game-info">game-info</div>
+      <div className="game-info">
+        game-info
+        {/*  */}
+      </div>
     </div>
   );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -6,36 +6,6 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
-  const calculateWinner = squares => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
-  }
-
   const handleClick = i => {
     const newSquares = squares.slice();
 
